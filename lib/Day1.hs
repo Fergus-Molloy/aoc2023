93a25deaeee0e81ef4d275664351fe1dc6337550
Fergus Molloy <fergus@molloy.xyz>
2023-12-01 14:14:59 +0000

    day 1 pt 1

--- lib/Day1.hs
+++ lib/Day1.hs
@@ -1,5 +1,35 @@
-module Day1(pt1,pt2) where
+module Day1 (pt1, pt2, parseFirstAndLast) where
 
-pt1 _ = 0
+import AocParse
+import Control.Applicative
+import Data.Char
+
+parseNumber :: Parser Char
+parseNumber = matchWhile (not . isDigit) *> matchDigit
+
+parseFirstAndLast :: Parser Int
+parseFirstAndLast = Parser f
+  where
+    f inp = do
+      (_, h) <- runParser parseNumber inp
+      (_, t) <- runParser parseNumber (reverse inp)
+      Right ("", read [h, t])
+
+parse :: Parser [Int]
+parse = Parser f
+  where
+    f inp = do
+      (_, ls) <- runParser (many matchLine) inp
+      nums <- mapM (runParser parseFirstAndLast) ls
+      Right ("", map snd nums)
+
+pt1 :: String -> Either String Int
+pt1 inp = do
+  (_, nums) <- runParser parse inp
+  Right $ sum nums
+
+-- (_, parsed) <- runParser parse inp
+-- Right $ trace ("parsed: " ++ show parsed) (sum parsed)
+
+pt2 :: String -> Int
 pt2 _ = 0
-
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,11 @@
 module Main where
 
+import Day1 qualified as D1
+import Main.Utf8 qualified as Utf8
+
 main :: IO ()
 main = do
-    putStr "Hello worlds"
+  d1 <- readFile "./inputs/day1"
+  Utf8.withUtf8 $ do
+    putStrLn $ "Day1 Pt1: " <> show (D1.pt1 d1)
+    putStrLn $ "Day1 Pt2: " <> show (D1.pt2 d1)
