96b7132f89762b0f1a1b542ac4c0d2b9ebc2c25a
Fergus Molloy <fergus@molloy.xyz>
2023-12-04 11:12:30 +0000

    add time and generate output strings

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # aoc2023
 
-
+|Day|Stars|Time|Cululative|
+|---|---|----|----|
+|1|**|10.5ms|10.5ms|
--- src/Main.hs
+++ src/Main.hs
@@ -7,5 +7,12 @@ main :: IO ()
 main = do
   d1 <- readFile "./inputs/day1"
   Utf8.withUtf8 $ do
-    putStrLn $ "Day1 Pt1: " <> show (D1.pt1 d1)
-    putStrLn $ "Day1 Pt2: " <> show (D1.pt2 d1)
+    putStrLn $ printAns (zip genDays [D1.pt1 d1, D1.pt2 d1])
+
+genDays :: [String]
+genDays = ["Day " <> show x <> " Pt" <> show y <> " -> " | x <- [1 ..] :: [Int], y <- [1, 2] :: [Int]]
+
+printAns :: [(String, Either String Int)] -> String
+printAns [] = ""
+printAns (h : t) =
+  fst h <> show (snd h) <> "\n" <> printAns t
