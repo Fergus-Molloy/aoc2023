777e6f818e3987fa6eb273afeec4a10f74746966
Fergus Molloy <fergus@molloy.xyz>
2023-12-04 12:58:07 +0000

    add cli to run specific day

---
+++ lib/Day2.hs
@@ -0,0 +1,7 @@
+module Day2 (pt1, pt2) where
+
+pt1 :: String -> Either String Int
+pt1 _ = Right 0
+
+pt2 :: String -> Either String Int
+pt2 _ = Right 0
--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,44 @@
 module Main where
 
 import Day1 qualified as D1
+import Day2 qualified as D2
 import Main.Utf8 qualified as Utf8
+import System.Environment
+
+type Day = Int
 
 main :: IO ()
 main = do
-  d1 <- readFile "./inputs/day1"
-  Utf8.withUtf8 $ do
-    putStrLn $ printAns (zip genDays [D1.pt1 d1, D1.pt2 d1])
+  args <- getArgs
+  d1 <- getInput 1
+  case args of
+    [] -> Utf8.withUtf8 $ do
+      putStrLn $ printAns (zip genDays [D1.pt1 d1, D1.pt2 d1])
+    (day : _) -> do
+      inp <- getInput d
+      putStrLn $ foldl (\b a -> b <> getDayString d (snd a) <> show (fst a) <> "\n") "" (zip (runDay d inp) [1 .. 2])
+      where
+        d = read day
+
+solutions :: [String -> Either String Int]
+solutions = [D1.pt1, D1.pt2, D2.pt1, D2.pt2]
+
+getSolutions :: Day -> [String -> Either String Int]
+getSolutions d = solutions !! idx : [solutions !! (idx + 1)]
+  where
+    idx = (d - 1) * 2
+
+getInput :: Day -> IO String
+getInput d = readFile $ "./inputs/day" <> show d
+
+runDay :: Day -> String -> [Either String Int]
+runDay d input = map (\f -> f input) (getSolutions d)
+
+getDayString :: Day -> Int -> String
+getDayString d p = "Day " <> show d <> " Pt" <> show p <> " -> "
 
 genDays :: [String]
-genDays = ["Day " <> show x <> " Pt" <> show y <> " -> " | x <- [1 ..] :: [Int], y <- [1, 2] :: [Int]]
+genDays = [getDayString x y | x <- [1 ..] :: [Int], y <- [1, 2] :: [Int]]
 
 printAns :: [(String, Either String Int)] -> String
 printAns [] = ""
