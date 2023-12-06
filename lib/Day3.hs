daccceffe471cc081cab52825d86e2cc4d5489fc
Fergus Molloy <fergus@molloy.xyz>
2023-12-06 13:45:59 +0000

    day 3 pt2 attempt

--- lib/Day3.hs
+++ lib/Day3.hs
@@ -1,10 +1,13 @@
 module Day3 where
 
 import Data.Char hiding (isSymbol)
+import Data.List
 import Debug.Trace
 import Prelude hiding (getChar)
 
 type Point = (Int, Int)
+
+-- tinp = "467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598.."
 
 getOrdinalIndicies :: Point -> [Point]
 getOrdinalIndicies (x, y) =
@@ -33,6 +36,13 @@ getNextNumber s = splitAt count $ drop d s
 getChar :: [String] -> Point -> Char
 getChar s (x, y) = (s !! y) !! x
 
+nextToDigits :: [String] -> Point -> [Point]
+nextToDigits s point = filter (isDigit . getChar s) $ filter inRange $ getOrdinalIndicies point
+  where
+    inRange (x, y) = x >= 0 && y >= 0 && x < w && y < h
+    w = length (s !! 0)
+    h = length s
+
 -- | are any of the chars next to a symbol (includes diagonals)
 nextToSymbol :: [String] -> [(Char, Point)] -> Bool
 nextToSymbol s =
@@ -45,9 +55,21 @@ nextToSymbol s =
           )
     )
   where
-    inRange (x, y) = x >= 0 && y >= 0 && x < w && y < length s
-    -- w = 10 -- test width
-    w = 140 -- actual input
+    inRange (x, y) = x >= 0 && y >= 0 && x < w && y < h
+    w = length (s !! 0)
+    h = length s
+
+getGears :: [String] -> [(Char, Point)] -> [Point]
+getGears s cs = concatMap (nextToGear s) cs
+
+-- | are any of the chars next to a symbol (includes diagonals)
+nextToGear :: [String] -> (Char, Point) -> [Point]
+nextToGear s (_, point) = gears
+  where
+    gears = filter ((==) '*' . getChar s) $ filter inRange $ getOrdinalIndicies point
+    inRange (x, y) = x >= 0 && y >= 0 && x < w && y < h
+    w = length (s !! 0)
+    h = length s
 
 -- | zip chars with thier coords
 stringWCoords :: String -> [[(Char, Point)]]
@@ -63,10 +85,33 @@ getAllNums acc s = case getNextNumber s of
 extractNums :: String -> [[(Char, Point)]]
 extractNums s = concatMap (filter (/= []) . getAllNums []) (stringWCoords s)
 
+-- | adjacent as in next to each other
+adjacentPoints :: Point -> Point -> Bool
+adjacentPoints (a, y1) (b, y2) = a - b == 1 || b - a == 1 || (a == b && y1 == y2)
+
+getNumFromPoint :: [String] -> Point -> Int
+getNumFromPoint s p = read $ getPre s p <> getPost s p
+  where
+    getPost ls (x, y) = takeWhile isDigit $ drop x (ls !! y)
+    getPre ls (x, y) = reverse $ takeWhile isDigit $ reverse $ take x (ls !! y)
+
 pt1 :: String -> Either String Int
 pt1 inp = Right $ sum $ map (read . map fst) ns
   where
     ns = filter (nextToSymbol ls) (extractNums inp)
     ls = lines inp
 
-pt2 _ = Right 0
+duplicates :: [Point] -> [Point]
+duplicates = duplicates' []
+
+duplicates' :: [Point] -> [Point] -> [Point]
+duplicates' acc [] = acc
+duplicates' acc [a] = a : acc
+duplicates' acc (a : ps) = if adjacentPoints a (head ps) then duplicates' acc ps else duplicates' (a : acc) ps
+
+pt2 :: String -> Either String Int
+pt2 inp = Right $ sum $ map product (trace ("Got nums: " <> show nums) nums)
+  where
+    nums = filter (\l -> length l >= 2) $ map ((nub . map (getNumFromPoint ls)) . nextToDigits ls) gears
+    gears = nub $ concat $ group $ sort (concatMap (getGears ls) (extractNums inp))
+    ls = lines inp
---
+++ lib/Day6.hs
@@ -0,0 +1,5 @@
+module Day6 where
+
+pt1 _ = 0
+
+pt2 _ = 0
